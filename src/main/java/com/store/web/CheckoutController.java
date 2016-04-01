031ed646558aa087b56a1a4db06ad6d9f8391831
Dariusz Dybka <dybka.dariusz@gmail.com>
2016-04-01 14:19:29 +0200

    Dodałem 2 kontrolery

---
+++ src/main/java/com/store/web/CheckoutController.java
@@ -0,0 +1,18 @@
+package com.store.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CheckoutController {
+
+    @RequestMapping(value = "/checkout", method = RequestMethod.GET)
+    public String checkout(Model model) {
+
+        return "checkout";
+    }
+
+}
